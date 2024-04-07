668acf400e727646182456885b5d7330b9070951
Amal K <12756149+1Amal@users.noreply.github.com>
2024-04-08 09:27:14 +1000

    Setup project Webpack files and configuration

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # WebPack-Restaurant-Page
 Practicing DOM manipulation by dynamically rendering a restaurant homepage
+
+Going to be using JavaScript alone to generate the entire contents of the website!
+
+
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Custom template',
+      // Load a custom template (lodash by default)
+      template: './src/index.html'
+    })
+  ]};
+
+
+
