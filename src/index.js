3ed33f9584ffa4e048cc9a1ffe4632334fa9e5a2
Amal K <12756149+1Amal@users.noreply.github.com>
2024-04-09 12:54:15 +1000

    Add method to clear screen for tabbed browsing

--- src/index.js
+++ src/index.js
@@ -22,29 +22,32 @@ document.querySelector("#Home").addEventListener('click',function(){
 
 function home()
 {
+    // content.removeChild(menuDiv);    
 
-    
+    const homeDiv=document.createElement("div");
+    homeDiv.setAttribute('id', 'homeDiv');
+    content.appendChild(homeDiv);
     
     const introHeading=document.createElement("h1");
     introHeading.textContent="About Star Trek Restaurant";
-    content.appendChild(introHeading);
+    homeDiv.appendChild(introHeading);
     
     const intro=document.createElement("p");
     intro.textContent="Established in the year 2024 in the vibrant city of Melbourne, the StarTrek restaurant has quickly become a beloved destination for fans of the iconic Star Trek: The Next Generation TV series. Situated in the heart of the city, this restaurant is a homage to the futuristic adventures of the USS Enterprise and its crew.From the moment patrons step through the doors of the StarTrek restaurant, they are transported into the world of Captain Jean-Luc Picard, Commander William Riker, and the rest of the Enterprise-D crew. The interior of the restaurant is adorned with memorabilia and decor inspired by the show, creating an immersive experience for restaurants.The menu at StarTrek restaurant is a culinary journey through the beloved series, with each dish carefully crafted to pay homage to the characters and themes of Star Trek: The Next Generation. From classic favorites to creative interpretations, there's something for every fan to enjoy. Guests can start their meal with Captain Picard's Earl Grey Tea, a comforting blend of tea that pays tribute to the captain's signature beverage. For appetizers, they might indulge in Riker's Ribs, tender and succulent ribs slow-cooked to perfection and served with a tangy barbecue sauce.For the main course, options abound. Worf's Klingon Gagh offers a playful twist on the traditional Klingon delicacy, while Data's Circuit Fries provide a crunchy and satisfying snack inspired by the android's mechanical nature. And of course, no trip to StarTrek restaurant would be complete without trying The Borg Burger, a futuristic take on the classic burger featuring unique toppings and flavors. To round off the meal, guests can satisfy their sweet tooth with Beverly's Blue Jello, a nostalgic treat that harks back to scenes from the Enterprise's mess hall. Whether it's a casual meal with friends or a themed gathering for die-hard Trekkies, StarTrek restaurant offers a dining experience like no other. With its delicious food, immersive atmosphere, and unwavering dedication to all things Star Trek, it's no wonder that fans keep coming back to boldly dine where no one has dined before."
     
-    content.appendChild(intro);
+    homeDiv.appendChild(intro);
     
     const OpenTimesHeading=document.createElement("h1");
     OpenTimesHeading.textContent="Opening Times";
-    content.appendChild(OpenTimesHeading);
+    homeDiv.appendChild(OpenTimesHeading);
     
     const openTimes = document.createElement("p");
     openTimes.textContent="Sunday: 8am - 8pm <br> Monday: 6am - 6pm <br> Tuesday: 6am - 6pm <br> Wednesday: 6am - 6pm <br> Thursday: 6am - 10pm <br> Friday: 6am - 10pm <br> Saturday: 8am - 10pm";
-    content.appendChild(openTimes);
+    homeDiv.appendChild(openTimes);
     
     const location=document.createElement("h1");
     location.textContent="Location: 10 Bourke Street, Melbourne";
-    content.appendChild(location);
+    homeDiv.appendChild(location);
 
 }
 
@@ -58,9 +61,15 @@ document.querySelector("#Menu").addEventListener('click',function(){
 
 function menuGenerate ()
 {
+    content.removeChild(homeDiv);
+
+    const menuDiv=document.createElement("div");
+    menuDiv.setAttribute('id','menuDiv');
+    content.appendChild(menuDiv);
+
     const menuHeading=document.createElement("h1");
     menuHeading.textContent="Menu";
-    content.appendChild(menuHeading);
+    menuDiv.appendChild(menuHeading);
 
     const menuItems=["Captain Picard's Earl Grey Tea: A comforting blend of Earl Grey tea, just like Captain Picard enjoys on the bridge of the USS Enterprise. Served hot. - $2.50",
     "Riker's Ribs: Tender and juicy ribs slow-cooked to perfection, just like Commander Riker would enjoy on shore leave. Served with your choice of barbecue sauce. - $15.99" ,
@@ -85,7 +94,7 @@ menuItems.forEach(item => {
     li.textContent = item;
     // Append <li> to <ol>
     menu.appendChild(li);
-    content.appendChild(menu);
+    menuDiv.appendChild(menu);
 
 });
 
